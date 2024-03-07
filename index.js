77e517acc38438137aeb79c2e5d872c9c0a401c6
is0280fp <umeyume45@gmail.com>
2024-03-08 08:32:34 +0900

    Open accordion

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const labels = document.querySelectorAll(".Menu-Item-Label")
 labels.forEach((label)=> {
     label.addEventListener("click", (e) => {
-        const targetContent = e.currentTarget.nextElementSibling;
-        targetContent.classList.toggle("Menu-ItemIsOpen")
+        const label = e.currentTarget
+        const itemInner = label.nextElementSibling
+        if (label.dataset.isOpen === "false") {
+            // const targetContent = e.currentTarget.nextElementSibling;
+            // targetContent.classList.toggle("Menu-ItemIsOpen")
+            const content = itemInner.children.item(itemInner.children.length-1)
+            console.log(content)
+            const targetHeight = content.getBoundingClientRect().top
+            console.log(targetHeight)
+            itemInner.style.height = `${targetHeight}px`
+        }
+
+        if (label.dataset.isOpen === "true") {
+            itemInner.style.height = "auto"
+            label.dataset.isOpen = true
+        }
+       
     })
 })
