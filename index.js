cda6210bf41fc28f9847f487bf80d0ff00fd4fbc
is0280fp <umeyume45@gmail.com>
2024-03-08 08:42:23 +0900

    fix height for dropdown

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ labels.forEach((label)=> {
         const label = e.currentTarget
         const itemInner = label.nextElementSibling
         if (label.dataset.isOpen === "false") {
-            // const targetContent = e.currentTarget.nextElementSibling;
-            // targetContent.classList.toggle("Menu-ItemIsOpen")
-            const content = itemInner.children.item(itemInner.children.length-1)
-            console.log(content)
-            const targetHeight = content.getBoundingClientRect().top
+            const lastContent = itemInner.children.item(itemInner.children.length-1)
+            const firstContent = itemInner.children[0]
+            const targetHeight = lastContent.getBoundingClientRect().bottom - firstContent.getBoundingClientRect().top
             console.log(targetHeight)
             itemInner.style.height = `${targetHeight}px`
+            // label.dataset.isOpen = true
         }
 
         if (label.dataset.isOpen === "true") {
             itemInner.style.height = "auto"
-            label.dataset.isOpen = true
+            label.dataset.isOpen = false
         }
        
     })
