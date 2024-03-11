a49ad68cdb8c123435a5319ade7a543da27e5e1d
is0280fp <umeyume45@gmail.com>
2024-03-11 09:35:21 +0900

    Fix bug

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ labels.forEach((label)=> {
     label.addEventListener("click", (e) => {
         const label = e.currentTarget
         const itemInner = label.nextElementSibling
-        if (label.dataset.isOpen === "false") {
+        const isOpen = label.dataset.isOpen === "true"
+
+        if (!isOpen) {
             const lastContent = itemInner.children.item(itemInner.children.length-1)
             const firstContent = itemInner.children[0]
             const targetHeight = lastContent.getBoundingClientRect().bottom - firstContent.getBoundingClientRect().top
-            console.log(targetHeight)
             itemInner.style.height = `${targetHeight}px`
             label.dataset.isOpen = true
+        } else {
+            itemInner.style.height = 0;
+            label.dataset.isOpen = false;
         }
-
-        if (label.dataset.isOpen === "true") {
-            // itemInner.style.height = "auto"
-            // itemInner.style.height = 0
-            label.dataset.isOpen = false
-        }
-       
     })
 })
